d64f9034c7cc22138e95bb8fe5a343b6283156cc
Nicolas <nicolasgfalabella@gmail.com>
2025-02-24 09:24:55 -0300

    Intruduccion

---
+++ introduccionJava/src/src/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo{
+    //Agregamos metodo principal main
+    public static void main(String[] args) {
+        System.out.println("Primer programa");
+        System.out.println("Saludos");
+    }
+}
