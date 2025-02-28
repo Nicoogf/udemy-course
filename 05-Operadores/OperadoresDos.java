4ac824b0bd24c8481877d510fd69d0d02db39cc8
Nicolas <nicolasgfalabella@gmail.com>
2025-02-28 15:29:00 -0300

    Operadores logicos

---
+++ 05-Operadores/OperadoresDos.java
@@ -0,0 +1,18 @@
+public class OperadoresDos {
+    public static void main(String[] args) {
+        var a = 14 ;
+        var b = 17 ;
+
+        var resultado = a < b ;
+        System.out.println(resultado);
+
+        var nuevoValor = !resultado ;
+
+
+        boolean c =  true , d = false ;
+
+        var nuevaVariable = c && d ;
+        System.out.println(nuevaVariable);
+
+    }
+}
