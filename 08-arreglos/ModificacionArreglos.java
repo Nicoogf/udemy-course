4d0d1fe7996c2a3f61f28a15e309cab245275ac9
Nicolas <nicolasgfalabella@gmail.com>
2025-03-08 10:44:55 -0300

    Arreglos

---
+++ 08-arreglos/ModificacionArreglos.java
@@ -0,0 +1,5 @@
+public class ModificacionArreglos {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 08-arreglos/introArreglo.java
@@ -0,0 +1,19 @@
+public class introArreglo {
+    public static void main(String[] args) {
+      int[] arreglo ;
+      arreglo = new int[5] ;
+
+      arreglo[0] = 1 ;
+      arreglo[1] = 2 ;
+      arreglo[2] = 3 ;
+      arreglo[3] = 4 ;
+      arreglo[4] = 5 ;
+
+     for( int i = 0 ; i < arreglo.length; i++){
+         System.out.println("El valor del indice [ " + i + " ] es : " + arreglo[i]);
+     }
+
+     int[] pruebaUno =  new int[3] ;
+     var numero =  new int[2] ;
+    }
+}
