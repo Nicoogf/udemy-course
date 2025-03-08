6c78dd1bc3df9b86af9b2e06a3c4e87af6ada6d5
Nicolas <nicolasgfalabella@gmail.com>
2025-03-08 10:57:26 -0300

    Lectura de elementos

--- 08-arreglos/ModificacionArreglos.java
+++ 08-arreglos/ModificacionArreglos.java
@@ -1,5 +1,16 @@
 public class ModificacionArreglos {
     public static void main(String[] args) {
 
+        var arregloPrueba = new int[5] ;
+        arregloPrueba[0] = 4;
+        arregloPrueba[1] = 4;
+        arregloPrueba[2] = 4;
+        arregloPrueba[3] = 4;
+        arregloPrueba[4] = 4;
+       //  arregloPrueba[5] = 20 ;
+
+       //  System.out.println(arregloPrueba[5]);
+
+
     }
 }
