21a26775680b238d40aafed1bc91abf83b344a3b
Nicolas <nicolasgfalabella@gmail.com>
2025-03-11 22:14:00 -0300

    Intro funciones

---
+++ 09-funciones/IntroFunciones.java
@@ -0,0 +1,10 @@
+public class IntroFunciones {
+
+    static void SaludoPractica(String nombre) {
+        System.out.println("Introduccion a Funciones");
+        System.out.println("Bienvenido usuario : " + nombre );
+    }
+    public static void main(String[] args) {
+        SaludoPractica("bradley08");
+    }
+}
