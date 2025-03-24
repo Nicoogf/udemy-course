fa0b0d3f9df669c0d025f7472d8f2a863f59cb6a
Nicolas <nicolasgfalabella@gmail.com>
2025-03-24 19:29:09 -0300

    Practica lunes

--- 10-POO/spotify/Prueba.java
+++ 10-POO/spotify/Prueba.java
@@ -3,7 +3,12 @@ package spotify;
 public class Prueba {
     public static void main(String[] args) {
 
+        Song cancion_uno = new Song("Jijiji" , "Patricio Rey" , 342) ;
 
+        String info_cancion_uno = cancion_uno.toString() ;
+        System.out.println(info_cancion_uno);
+
+        System.out.println("Actualmente se crearon :  " + Song.getCantidadDeCanciones() + " Canciones ");
 
     }
 }
--- 10-POO/spotify/Song.java
+++ 10-POO/spotify/Song.java
@@ -1,34 +1,20 @@
 package spotify ;
 
 public class Song {
-
+    private int idSong ;
     private String name ;
     private String artist ;
-    private int duration ;
-    private static int songCounter = 0;
+    private int duration ; // En segundos
 
-    public int getDuration() {
-        return duration;
-    }
+    private static int cantidadDeCanciones = 0;
+    private static int nextID = 1;
 
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public static int getSongCounter() {
-        return songCounter;
-    }
-
-    public static void setSongCounter(int songCounter) {
-        Song.songCounter = songCounter;
-    }
-
-    public String getArtist() {
-        return artist;
-    }
-
-    public void setArtist(String artist) {
-        this.artist = artist;
+    public Song( String name , String artist , int duration ) {
+        this.idSong = nextID++ ;
+        this.name =  name ;
+        this.artist = artist ;
+        this.duration = duration ;
+        cantidadDeCanciones ++ ;
     }
 
     public String getName() {
@@ -39,11 +25,47 @@ public class Song {
         this.name = name;
     }
 
-    public Song(String name , String artist , int duration ) {
-        this.name = name ;
-        this.artist = artist ;
-        this.duration = duration ;
-        songCounter ++ ;
+    public int getIdSong() {
+        return idSong;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public static int getCantidadDeCanciones() {
+        return cantidadDeCanciones;
+    }
+
+    public static int getNextID() {
+        return nextID;
+    }
+
+    public static void setNextID(int nextID) {
+        Song.nextID = nextID;
+    }
+
+    @Override
+    public String toString() {
+        String resultado =  "Cancion : " + this.idSong + "\n";
+        resultado += "Nombre : " + this.name + "\n" ;
+        resultado += "Artista : " + this.artist + "\n" ;
+        resultado += "Duracion : " + this.duration + " segundos " + "\n";
+        resultado += "Cantidad de canciones creadas : " + Song.cantidadDeCanciones ;
+
+        return resultado ;
     }
 
 }
