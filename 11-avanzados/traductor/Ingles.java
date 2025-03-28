0934cf52b009958bb4ce4ff76835426353f66885
Nicolas <nicolasgfalabella@gmail.com>
2025-03-28 20:05:23 -0300

    Viernes: interfaz

--- 11-avanzados/traductor/Ingles.java
+++ 11-avanzados/traductor/Ingles.java
@@ -3,6 +3,6 @@ package traductor;
 class Ingles implements Traductor {
     @Override
     public void traducir(){
-        System.out.println("Traduzco ingles");
+        System.out.println("Traduzco ingles desde interfaz");
     }
 }
