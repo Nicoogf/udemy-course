163325dece4ba13f7f88db1cb3a74ebe0206c0b4
Nicolas <nicolasgfalabella@gmail.com>
2025-04-01 21:50:40 -0300

    Aplicacion snacks

--- 11-avanzados/proyecto/Snacks.java
+++ 11-avanzados/proyecto/Snacks.java
@@ -1,21 +1,35 @@
 package proyecto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Snacks {
-    private static List<Snack> snacksList ;
+    private static final List<Snack> snacks ;
 
-    public void AgregarSnack( Snack snackAgregado ){
-        snacksList.add(snackAgregado) ;
-        System.out.println("Snack agregado exitosamente");
+
+    static {
+        snacks = new ArrayList<>() ;
+        snacks.add(new Snack("Papas" , 70 )) ;
+        snacks.add(new Snack("Refresco" , 50 )) ;
+        snacks.add(new Snack("Sandwich" , 120 )) ;
     }
 
-    public void MostrarSnack() {
-        snacksList.forEach(System.out::println);
+    public static void agregarSnack( Snack snack) {
+        snacks.add( snack) ;
     }
 
-    public void GetSnacks(){
-
+    public static void MostrarSnack() {
+        var inventarioSnack = "" ;
+        for( var snack :  snacks){
+            inventarioSnack += snack.toString() + " \n ";
+        }
+        System.out.println("--- Snacks en el inventario ---");
+        System.out.println(inventarioSnack);
     }
+
+    public static List<Snack> getSnacks(){
+        return snacks ;
+    }
+
 
 }
